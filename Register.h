8e941c62da532bf03d245986ddee6fb677ba20d5
fariation <faraaugust@gmail.com>
2025-11-30 21:22:05 +0700

    Refactor Register.h to include Sepeda struct

--- Register.h
+++ Register.h
@@ -1,23 +1,32 @@
 #ifndef _REGISTER_H
 #define _REGISTER_H
+
 #include "Person.h"
-#include "Sepeda.h"
 #include <vector>
 #include <string>
+
+struct Sepeda
+{
+    int nomor = 0;
+    bool dipinjam = false;
+    std::string tanggalPinjam;
+    std::string nimPeminjam;
+};
 
 class Registrasi
 {
 private:
     std::vector<Mahasiswa> data;
-    std::string sepedaDipinjamNomor;
-    std::string sepedaTanggalPinjam;
-    bool sepedaSedangDipinjam = false;
+    std::vector<Sepeda> daftarSepeda;
 
 public:
+    Registrasi() = default;
+
     void registrasiMahasiswa();
     void lihatDataMahasiswa();
     void editDataMahasiswa();
     void hapusDataMahasiswa();
+
     void peminjamanSepeda();
     void pengembalianSepeda();
 };
